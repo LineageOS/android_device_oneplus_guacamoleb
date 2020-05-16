b112d7a8aa410a211b592bd4160ed71c861ce949
LuK1337 <priv.luk@gmail.com>
2020-05-16 14:53:45 +0200

    guacamoleb: Set correct TARGET_SCREEN_DENSITY
    Change-Id: Iea8d19d1064c7313eb47ad6a88aa79f04f01116a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@ DEVICE_PATH := device/oneplus/guacamoleb
 TARGET_OTA_ASSERT_DEVICE := OnePlus7
 
 # Display
-TARGET_SCREEN_DENSITY := 560
+TARGET_SCREEN_DENSITY := 480
 
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/sm8150-perf_defconfig
