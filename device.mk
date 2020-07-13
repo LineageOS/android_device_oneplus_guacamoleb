42b5312ab806d2f43bf2831f34be4cafb78eea3e
Ashwin R C <ashwin2001achu@gmail.com>
2020-07-13 15:49:57 +0000

    guacamoleb: drop unused mixer_paths_11811.xml
    * Stock builds dont have these anymore
    * None of the blobs reference it
    
    Change-Id: Ib37e13c26226c7968acd89dfa7e7e587a6bbebc3

--- device.mk
+++ device.mk
@@ -19,7 +19,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/mixer_paths_11811.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_11811.xml \
     $(LOCAL_PATH)/audio/mixer_paths_pahu.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_pahu.xml \
     $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml
 
