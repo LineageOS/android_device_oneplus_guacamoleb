db65a8048a595bb7e4e6e703828b6406c439c044
Bruno Martins <bgcngm@gmail.com>
2021-09-20 18:23:58 +0100

    guacamoleb: Blob out ODM feature list
    There's not much of a purpose to have this in the device tree.
    
    Change-Id: I67751642b2f34576106a39a83e21996010215c3d

--- device.mk
+++ device.mk
@@ -26,10 +26,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fstab.qcom
 
-# OPFeature
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/odm_feature_list:$(TARGET_COPY_OUT_ODM)/etc/odm_feature_list
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
