94975c18e25fcb8103ca209f64327c569846b778
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 22:09:41 -0700

    guacamole: overlay: Convert to RRO packages
    Change-Id: Iae6ef9fa9743bd2447991ad11d38fa184b12981d

--- device.mk
+++ device.mk
@@ -25,8 +25,12 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_PACKAGES += \
+    OPlusFrameworksResTarget \
+    OPlusSettingsProviderResTarget \
+    OPlusSystemUIResTarget
 
 # Shipping API
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
