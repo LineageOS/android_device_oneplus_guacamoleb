d4cdbbec5d9136ab290b2b3f91935920eb2d37d6
LuK1337 <priv.luk@gmail.com>
2022-09-27 09:22:45 +0200

    guacamoleb: Align with OnePlus SM8250
    Change-Id: I107735ae6be71f570e0d036f9c2a4ffbd1cd9c5d

--- lineage_guacamoleb.mk
+++ lineage_guacamoleb.mk
@@ -17,26 +17,18 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_guacamoleb
 PRODUCT_DEVICE := guacamoleb
-PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := GM1905
 PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_MODEL := GM1905
+PRODUCT_BRAND := OnePlus
 
 PRODUCT_SYSTEM_NAME := OnePlus7
 PRODUCT_SYSTEM_DEVICE := OnePlus7
 
-PRODUCT_AAPT_CONFIG := xxhdpi
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-PRODUCT_CHARACTERISTICS := nosdcard
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2240
-TARGET_SCREEN_WIDTH := 1080
-
-# Build info
-BUILD_FINGERPRINT := "OnePlus/OnePlus7/OnePlus7:10/QKQ1.190716.003/2003131900:user/release-keys"
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=OnePlus7 \
-    TARGET_PRODUCT=OnePlus7 \
-    PRIVATE_BUILD_DESC="OnePlus7-user 10 QKQ1.190716.003 2003131900 release-keys"
-
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="OnePlus7-user 10 QKQ1.190716.003 2003131900 release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := OnePlus/OnePlus7/OnePlus7:10/QKQ1.190716.003/2003131900:user/release-keys
